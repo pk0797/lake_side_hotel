25bff94fb0c132ca6690d9b616efc0639632d5c0
pk0797 <prem.k.kumar97@gmail.com>
2024-07-21 15:01:00 +0530

    file helps to edit room

---
+++ Client/LakeSide-hotel/src/components/room/EditRoom.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EditRoom = () => {
+  return (
+    <div>
+        
+        <h2>
+        EditRoom
+        </h2>
+
+    </div>
+  )
+}
+
+export default EditRoom
