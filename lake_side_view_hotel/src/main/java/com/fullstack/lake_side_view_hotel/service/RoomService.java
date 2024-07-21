8197da090f50d5517807493a92b6340889df564b
pk0797 <prem.k.kumar97@gmail.com>
2024-07-21 15:04:38 +0530

    Room Service File has been updated

--- lake_side_view_hotel/src/main/java/com/fullstack/lake_side_view_hotel/service/RoomService.java
+++ lake_side_view_hotel/src/main/java/com/fullstack/lake_side_view_hotel/service/RoomService.java
@@ -1,5 +1,6 @@
 package com.fullstack.lake_side_view_hotel.service;
 
+import com.fullstack.lake_side_view_hotel.exception.ResourceNotFoundException;
 import com.fullstack.lake_side_view_hotel.model.Room;
 import com.fullstack.lake_side_view_hotel.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +38,32 @@ public class RoomService implements IRoomService{
     @Override
     public List<String> getAllRoomTypes() {
         return roomRepository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomPhotoByRoomId(Long roomId) throws SQLException {
+        Optional<Room> theRoom = roomRepository.findById(roomId);
+        if(theRoom.isEmpty()){
+            throw new ResourceNotFoundException("Sorry, Room not found!");
+        }
+        Blob photoBlob = theRoom.get().getPhoto();
+        if(photoBlob != null){
+            return photoBlob.getBytes(1,(int) photoBlob.length());
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteRoom(Long roomId) {
+        Optional<Room> theRoom = roomRepository.findById(roomId);
+        if(theRoom.isPresent()){
+            roomRepository.deleteById(roomId);
+        }
     }
 }
 
