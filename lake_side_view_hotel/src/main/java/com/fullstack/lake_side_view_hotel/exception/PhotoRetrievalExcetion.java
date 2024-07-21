9e2c07c84a0c0406a7befc29cf269090abb1bb1e
pk0797 <prem.k.kumar97@gmail.com>
2024-07-21 15:03:21 +0530

    custom exception handling file

---
+++ lake_side_view_hotel/src/main/java/com/fullstack/lake_side_view_hotel/exception/PhotoRetrievalExcetion.java
@@ -0,0 +1,7 @@
+package com.fullstack.lake_side_view_hotel.exception;
+
+public class PhotoRetrievalExcetion extends RuntimeException {
+    public PhotoRetrievalExcetion(String message) {
+        super(message);
+    }
+}
