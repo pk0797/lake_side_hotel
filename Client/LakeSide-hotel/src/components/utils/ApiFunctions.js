85420f2b389aa6b3467e94e1bcdb21f8aa2e770d
pk0797 <prem.k.kumar97@gmail.com>
2024-07-22 20:22:26 +0530

    Front end API function implemented for bookRoom, allbooking, getBooking by confirmation code and cancel booking

--- Client/LakeSide-hotel/src/components/utils/ApiFunctions.js
+++ Client/LakeSide-hotel/src/components/utils/ApiFunctions.js
@@ -82,3 +82,50 @@ export async function getRoomById(roomId) {
         throw new Error(`Error fetching room ${error.message}`)
     }
 }
+
+/*This functions saves a new booking to the database*/
+export async function bookRoom(roomId, booking){
+    try{
+        const response = await api.post(`/bookings/room/${roomId}/booking`, booking)
+        return response.data
+    }catch(error){
+    }   if(error.response && error.response.data){
+        throw new Error(error.response.data)
+    }else{
+        throw new Error(`Error booking room : ${error.message}`)
+    }
+}
+
+/* This function gets all the bookings from database */
+export async function getAllBookings(){
+    try{
+        const result = await api.get("/bookings/all-bookings")
+        return result.data
+    }catch(error){
+        throw new Error(`Error fetching bookings : ${error.message}`)
+    }
+}
+
+/*This function gets booking by the confirmation code  */
+export async function getBookingByConfirmationCode(confirmationCode){
+    try{
+        const result = await api.get(`/bookings/confirmation/${confirmationCode}`)
+        return result.data
+    }catch(error){
+        if(error.response && error.response.data){
+            throw new Error(error.response.data)
+        }else{
+            throw new Error(`Error find booking : ${error.message} `)
+        }
+    }
+}
+
+/*this function cancels bookings */
+export async function cancelBooking(bookingId){
+    try{
+        const result = await api.delete(`/bookings/booking/${bookingId}/delete`)
+        return result.data
+    }catch(error){
+        throw new Error (`Error cancelling booking :${error.message}`)
+    }
+}
